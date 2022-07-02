92d0aa12a0a8c6d01f5edc1dc0de0d8cccb17756
piesieomane <piesieyeboah@gmail.com>
2022-07-03 01:42:03 +0300

    xlasses

--- book.js
+++ book.js
@@ -3,49 +3,71 @@ const title = document.querySelector(".title");
 const author = document.querySelector(".author");
 const displayBooks = document.querySelector(".book-shelf");
 
-const collection = JSON.parse(localStorage.getItem("form")) || [];
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.id = Date.now();
+  }
 
+  collection = JSON.parse(localStorage.getItem("form")) || [];
+
+  addBook() {
+    if (!title.value && !author.value) return;
+    const book = {
+      author: this.author,
+      title: this.title,
+      id: this.id,
+    };
+    this.collection.push(book);
+    localStorage.setItem("form", JSON.stringify(this.collection));
+    title.value = "";
+    author.value = "";
+  }
+}
+
+class Books extends Book {
+  renderBooks = () => {
+    this.collection = JSON.parse(localStorage.getItem("form")) || [];
+    displayBooks.innerHTML = "";
+    this.collection.forEach((oneBook) => {
+      const renderBook = document.createElement("div");
+      renderBook.classList.add("class-book");
+      const p = document.createElement("p");
+      const info = document.createElement("div");
+      p.textContent = `"${oneBook.title}" by ${oneBook.author}`;
+      const removeBtn = document.createElement("button");
+      removeBtn.textContent = "Remove";
+      removeBtn.classList.add("remove-btn");
+      removeBtn.setAttribute("id", oneBook.id);
+      info.appendChild(p);
+      info.appendChild(removeBtn);
+      renderBook.appendChild(info);
+      displayBooks.appendChild(renderBook);
+    });
+  };
+
+  removeBook = (targetId) => {
+    const newArr = this.collection.filter((item) => item.id !== targetId);
+    this.collection.length = 0;
+    this.collection.push(...newArr);
+    localStorage.setItem("form", JSON.stringify(this.collection));
+    this.renderBooks();
+  };
+}
+
+////////////////////////////////////
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  if (!(author.value && title.value)) return null;
-  const book = { author: author.value, title: title.value, id: Date.now() };
-  collection.push(book);
-  localStorage.setItem("form", JSON.stringify(collection));
-  title.value = "";
-  author.value = "";
-  renderBooks();
+  const newBook = new Books(title, author, Date.now());
+  newBook.addBook();
+  newBook.renderBooks();
 });
-
-const renderBooks = () => {
-  displayBooks.innerHTML = "";
-  return collection.forEach((oneBook) => {
-    const renderBook = document.createElement("div");
-    renderBook.classList.add("class-book");
-    const p = document.createElement("p");
-    const info = document.createElement("div");
-    p.textContent = `"${oneBook.title}" by ${oneBook.author}`;
-    const removeBtn = document.createElement("button");
-    removeBtn.textContent = "Remove";
-    removeBtn.classList.add("remove-btn");
-    removeBtn.setAttribute("id", oneBook.id);
-    info.appendChild(p);
-    info.appendChild(removeBtn);
-    renderBook.appendChild(info);
-    displayBooks.appendChild(renderBook);
-  });
-};
-
+////////////////////////////////////
+const mixcBook = new Books();
 displayBooks.addEventListener("click", (e) => {
   if (e.target.classList.contains("remove-btn")) {
     const targetId = parseInt(e.target.getAttribute("id"), 10);
-    removeBook(targetId);
+    mixcBook.removeBook(targetId);
   }
 });
-
-const removeBook = (targetId) => {
-  const newArr = collection.filter((item) => item.id !== targetId);
-  collection.length = 0;
-  collection.push(...newArr);
-  localStorage.setItem("form", JSON.stringify(collection));
-  renderBooks();
-};
